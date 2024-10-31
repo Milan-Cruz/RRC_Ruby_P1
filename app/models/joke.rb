c1b7e27d2ac1f55d923946caf42938b6a2181ec2
Milan Cruz <milanmoreira@gmail.com>
2024-10-30 23:57:47 -0500

    Added validations for Joke and Category

--- app/models/joke.rb
+++ app/models/joke.rb
@@ -1,4 +1,7 @@
 class Joke < ApplicationRecord
   has_and_belongs_to_many :categories
+
+  # Validations
   validates :joke_text, presence: true, uniqueness: true
+  validates :api_id, presence: true, uniqueness: true
 end
