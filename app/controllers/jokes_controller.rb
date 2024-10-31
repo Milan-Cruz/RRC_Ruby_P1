f6619d3cad2de8940b8d534c636548b5ed74f390
Milan Cruz <milanmoreira@gmail.com>
2024-10-31 00:39:04 -0500

    Added pagination to jokes

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -1,9 +1,9 @@
 class JokesController < ApplicationController
   def index
     if params[:query].present?
-      @jokes = Joke.where("joke_text LIKE ?", "%#{params[:query]}%")
+      @jokes = Joke.where("joke_text LIKE ?", "%#{params[:query]}%").page(params[:page]).per(20)
     else
-      @jokes = Joke.all
+      @jokes = Joke.page(params[:page]).per(20)
     end
   end
 
