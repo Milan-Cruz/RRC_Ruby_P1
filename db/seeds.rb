e2e0932258a38a00f1e3f9fabfaf14a2f1113e66
Milan Cruz <milanmoreira@gmail.com>
2024-10-30 19:48:24 -0500

    API data seeded into database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,28 @@
+require "httparty"
+
+# Fetch all categories
+categories_response = HTTParty.get("https://api.chucknorris.io/jokes/categories")
+categories_response.each do |category_name|
+  Category.find_or_create_by!(name: category_name)
+end
+
+# Fetch and seed jokes for each category
+20.times do
+  joke_response = HTTParty.get("https://api.chucknorris.io/jokes/random")
+  joke = Joke.find_or_create_by!(
+    joke_text: joke_response["value"],
+    api_id: joke_response["id"],
+    icon_url: joke_response["icon_url"],
+    url: joke_response["url"],
+  )
+
+  # Assign categories
+  joke_response["categories"].each do |category_name|
+    category = Category.find_by(name: category_name)
+    joke.categories << category unless joke.categories.include?(category)
+  end
+end
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
