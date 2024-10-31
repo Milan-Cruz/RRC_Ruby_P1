6c9a2b655138e529fbd0ba6f437caa1c21e90fa3
Milan Cruz <milanmoreira@gmail.com>
2024-10-30 20:58:31 -0500

    Search Jokes added

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -1,5 +1,10 @@
 class JokesController < ApplicationController
   def index
+    if params[:query].present?
+      @jokes = Joke.where("joke_text LIKE ?", "%#{params[:query]}%")
+    else
+      @jokes = Joke.all
+    end
   end
 
   def show
